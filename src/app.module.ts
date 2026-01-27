f1e38c2cc42d0d20d283d000c1c48f4ab52fb607
C-Happy <happycyizere69@gmail.com>
2026-01-27 07:54:15 +0200

    Modules Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { SurveyModule } from './survey/survey.module';
+import { ResponseModule } from './response/response.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, AdminModule, SurveyModule, ResponseModule, AnalyticsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
